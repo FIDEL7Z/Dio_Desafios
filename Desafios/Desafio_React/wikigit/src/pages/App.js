53cbf32ddc87caa93bf7920af0575ccf9c748179
Jefferson Fidelis <junior.fidelis.3386@gmail.com>
2024-06-27 13:51:17 -0300

    novo projeto

---
+++ Desafios/Desafio_React/wikigit/src/pages/App.js
@@ -0,0 +1,13 @@
+import gitlogo from '../assets/github.png'
+
+import { Container } from './styles';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={gitlogo} width={72} height={72}/>
+    </div>
+  );
+}
+
+export default App;
